59b77e2f85f4462bdc271aeeed0b6099af1ad54d
diznx <85420656+diznx@users.noreply.github.com>
2022-03-29 06:42:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import yfinance
 tickersymbol1 = 'AMC'
 tickersymbol2 = 'GME'
 
+
 tickerData1 = yfinance.Ticker(tickersymbol1)
 tickerData2 = yfinance.Ticker(tickersymbol2)
 
@@ -14,7 +15,7 @@ tickerDf2 = tickerData2.info['regularMarketPrice']
 
 #main application window
 root = tkinter.Tk()
-root.title("stonks")
+root.title("stonk monitor")
 
 
 #creates a content frame
@@ -24,10 +25,14 @@ root.columnconfigure(1, weight=2)
 root.rowconfigure(1, weight=2)
 
 #content within window
-ttk.Label(mainframe, text="AMC").grid(column=1, row=1, sticky=tkinter.W)
+ttk.Label(mainframe, text="     AMC     ").grid(column=1, row=1, sticky=tkinter.W)
 ttk.Label(mainframe, text=tickerDf1).grid(column=2, row=1, sticky=tkinter.W)
-ttk.Label(mainframe, text='GME').grid(column=1, row=2, sticky=tkinter.W)
+ttk.Label(mainframe, text=round(tickerDf1 * 3614)).grid(column=3, row=1, sticky=tkinter.W)
+ttk.Label(mainframe, text="     ").grid(column=4, row=1, sticky=tkinter.W)
+ttk.Label(mainframe, text="     GME     ").grid(column=1, row=2, sticky=tkinter.W)
 ttk.Label(mainframe, text=tickerDf2).grid(column=2, row=2, sticky=tkinter.W)
+ttk.Label(mainframe, text=round(tickerDf2 * 12)).grid(column=3, row=2, sticky=tkinter.W)
+ttk.Label(mainframe, text="     ").grid(column=4, row=2, sticky=tkinter.W)
 
 
 #tells Tk to start events
